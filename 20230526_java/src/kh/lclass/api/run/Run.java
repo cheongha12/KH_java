d990fc55ef6f406da09c981bacd040b39a86786f
cheongha12 <cheong.9396@gmail.com>
2023-06-07 14:47:55 +0900

    API

---
+++ 20230526_java/src/kh/lclass/api/run/Run.java
@@ -0,0 +1,8 @@
+package kh.lclass.api.run;
+
+public class Run {
+public static void main(String[] args) {
+//	new TestApi().testString();
+	new TestApi().testWrapperDoWhile();
+}
+}
---
+++ 20230526_java/src/kh/lclass/vo/TestVojava.java
@@ -0,0 +1,10 @@
+package kh.lclass.vo;
+
+public class TestVojava {
+	private String boardWriter;
+	private String boardTitle;
+	private String boardContent;
+	private String boardData;
+	
+
+}
