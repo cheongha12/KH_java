9927b45602c8da48235336738eea30fc5c450daf
cheongha12 <cheong.9396@gmail.com>
2023-06-15 13:56:52 +0900

    .

--- 20230614_multichat_server/src/kh/lclass/chat/client/ClientRun.java
+++ 20230614_multichat_server/src/kh/lclass/chat/client/ClientRun.java
@@ -6,6 +6,7 @@ public class ClientRun {
 	public static void main(String[] args) {
 		//사용자 nickname을 GUI를 통해서 입력받음.
 		String nickName= JOptionPane.showInputDialog("닉네임을 입력해주세요.");
-		ClientGUI cgui = new ClientGUI(nickName);	//객체 생성==> 생성자에 화면초기화+event
+		
+		 new ClientGUI(nickName);	//객체 생성==> 생성자에 화면초기화+event
 }
 }
